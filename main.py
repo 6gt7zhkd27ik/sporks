78cb74fb5cce6eca78e23bdf81b6ee3300f8e9a8
6gt7zhkd27ik <6gt7zhkd27ik@proton.me>
2025-12-16 20:43:21 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import http.server
+import socketserver
+import webbrowser
+import urllib.request
+import os
+
+DUCKDNS_DOMAIN = "stlckmacsterluke"
+DUCKDNS_TOKEN = os.getenv("TOKEN")
+PORT = 8000
+
+if not DUCKDNS_TOKEN:
+    raise ValueError("No TOKEN!")
+
+url = f"https://www.duckdns.org/update?domains={DUCKDNS_DOMAIN}&token={DUCKDNS_TOKEN}&ip="
+with urllib.request.urlopen(url) as response:
+    print("DuckDNS response:", response.read().decode())
+
+webbrowser.open(f"http://localhost:{PORT}")
+socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler).serve_forever()
